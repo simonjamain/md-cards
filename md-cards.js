e246a19ab3721d25df8a01d85d6e97af82616eb6
Simon J <jamain.simon@gmail.com>
2016-03-11 09:53:18 +0100

    first commit

---
+++ md-cards.js
@@ -0,0 +1,27 @@
+(function() {
+  $('.material_card-thumnail.video').each(function() {
+
+    var youtubeId = $(this).data('youtubeid');
+    var backgroundUrl = 'http://img.youtube.com/vi/' + youtubeId + '/hqdefault.jpg';
+    //replace placeholder image by play icon
+    $(this).css({
+      'background-image':"url('http://placehold.it/100x100'),url('" + backgroundUrl + "')",
+      'background-size':'auto,cover',
+      'background-repeat':'no-repeat,no-repeat'
+      });
+
+    //check for auto adjust
+    var autoAdjustHeight = $(this).attr('auto-height');
+    if (typeof autoAdjustHeight !== typeof undefined && autoAdjustHeight !== false) {
+
+    }
+
+    $(this).one('click',function() {
+      // we assume youtube API is ready
+      var player = $('<iframe scrolling="no" class="player" width="'+$(this).width()+'" height="'+$(this).height()+'" src="https://www.youtube.com/embed/'+youtubeId+'?rel=0&autoplay=1&controls=0" frameborder="0" allowfullscreen id="yes"></iframe>').appendTo($(this));
+
+      var hoveringTitle = $(this).find('.material_card-title').first().clone().appendTo($(this)).addClass('hovering-video');
+
+      });
+  });
+})();
