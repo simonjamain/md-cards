07415174c4325c24588ed44babc17de12559438f
Simon J <jamain.simon@gmail.com>
2016-03-12 18:54:44 +0100

    fix dimensions of video thumbs and fix typo

--- md-cards.js
+++ md-cards.js
@@ -1,5 +1,5 @@
 (function() {
-  $('.material_card-thumnail.video').each(function() {
+  $('.material_card-thumbnail.video').each(function() {
 
     var youtubeId = $(this).data('youtubeid');
     var backgroundUrl = 'http://img.youtube.com/vi/' + youtubeId + '/hqdefault.jpg';
@@ -18,7 +18,7 @@
 
     $(this).one('click',function() {
       // we assume youtube API is ready
-      var player = $('<iframe scrolling="no" class="player" width="'+$(this).width()+'" height="'+$(this).height()+'" src="https://www.youtube.com/embed/'+youtubeId+'?rel=0&autoplay=1&controls=0" frameborder="0" allowfullscreen id="yes"></iframe>').appendTo($(this));
+      var player = $('<iframe scrolling="no" class="player" width="100%" height="100%" src="https://www.youtube.com/embed/'+youtubeId+'?rel=0&autoplay=1&controls=0" frameborder="0" allowfullscreen id="yes"></iframe>').appendTo($(this));
 
       var hoveringTitle = $(this).find('.material_card-title').first().clone().appendTo($(this)).addClass('hovering-video');
 
