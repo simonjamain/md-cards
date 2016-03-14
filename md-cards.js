3ee992cff9647448b3bcc547f9b255fd1015f6f8
Simon J <jamain.simon@gmail.com>
2016-03-14 10:42:31 +0100

    play icon added

--- md-cards.js
+++ md-cards.js
@@ -3,24 +3,26 @@
 
     var youtubeId = $(this).data('youtubeid');
     var backgroundUrl = 'http://img.youtube.com/vi/' + youtubeId + '/hqdefault.jpg';
-    //replace placeholder image by play icon
+
     $(this).css({
-      'background-image':"url('http://placehold.it/100x100'),url('" + backgroundUrl + "')",
-      'background-size':'auto,cover',
-      'background-repeat':'no-repeat,no-repeat'
+      'background-image':"url('" + backgroundUrl + "')",
+      'background-size':'cover',
+      'background-repeat':'no-repeat'
       });
 
     //check for auto adjust
     var autoAdjustHeight = $(this).attr('auto-height');
     if (typeof autoAdjustHeight !== typeof undefined && autoAdjustHeight !== false) {
-
+        //TODO
     }
 
-    $(this).one('click',function() {
+    //replace placeholder image by play icon
+    var frame = $(this);
+    $('<div class="material_card-play-icon material-icons">&#xE039;</div>').appendTo($(this)).one('click',function() {
       // we assume youtube API is ready
-      var player = $('<iframe scrolling="no" class="player" width="100%" height="100%" src="https://www.youtube.com/embed/'+youtubeId+'?rel=0&autoplay=1&controls=0" frameborder="0" allowfullscreen id="yes"></iframe>').appendTo($(this));
+      var player = $('<iframe scrolling="no" class="player" width="100%" height="100%" src="https://www.youtube.com/embed/'+youtubeId+'?rel=0&autoplay=1&controls=0" frameborder="0" allowfullscreen id="yes"></iframe>').appendTo(frame);
 
-      var hoveringTitle = $(this).find('.material_card-title').first().clone().appendTo($(this)).addClass('hovering-video');
+      var hoveringTitle = frame.find('.material_card-title').first().clone().appendTo(frame).addClass('hovering-video');
 
       });
   });
